9c677a62a6d02a85a0cc310c55407d596c6f5dbc
Mateo Olaya Bernal <mateo.olaya@globant.com>
2018-02-02 20:52:57 -0500

    Added CocoaPods dependecies manager

---
+++ blacklist/Pods/Target Support Files/Pods-blacklist/Pods-blacklist-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_blacklist : NSObject
+@end
+@implementation PodsDummy_Pods_blacklist
+@end
---
+++ blacklist/Pods/Target Support Files/Pods-blacklistTests/Pods-blacklistTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_blacklistTests : NSObject
+@end
+@implementation PodsDummy_Pods_blacklistTests
+@end
