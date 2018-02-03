094da8792127babae30c9bc54d7f9a047916309e
Mateo Olaya Bernal <mateo.olaya@globant.com>
2018-02-02 20:57:53 -0500

    Removed Pod folder

--- blacklist/Pods/Target Support Files/Pods-blacklist/Pods-blacklist-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_blacklist : NSObject
-@end
-@implementation PodsDummy_Pods_blacklist
-@end
--- blacklist/Pods/Target Support Files/Pods-blacklistTests/Pods-blacklistTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_blacklistTests : NSObject
-@end
-@implementation PodsDummy_Pods_blacklistTests
-@end
