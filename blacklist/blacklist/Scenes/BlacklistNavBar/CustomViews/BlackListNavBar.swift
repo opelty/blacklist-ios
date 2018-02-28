b155dd90802a861908adf8ab92d44760b46b6703
Mateo Olaya Bernal <mateo.olaya@globant.com>
2018-02-27 22:38:13 -0500

    Fixed back button issue in the blacklist navigation controller

--- blacklist/blacklist/Scenes/BlacklistNavBar/CustomViews/BlackListNavBar.swift
+++ blacklist/blacklist/Scenes/BlacklistNavBar/CustomViews/BlackListNavBar.swift
@@ -16,11 +16,15 @@ class BlackListNavBar: UINavigationController {
     let screenWidth = UIScreen.main.bounds.width
     let navBarTitleFont = StyleSheet.Font.lobsterRegular.rawValue
 
+    public private(set) var blacklistNavigationItem: UINavigationItem?
+    public private(set) var blacklistNavigationBar: UINavigationBar?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        delegate = self
+
         setUpNavBar()
         setUpStatusbar()
-
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -39,32 +43,32 @@ class BlackListNavBar: UINavigationController {
     }
 
     func setUpNavBar() {
-
         // MARK: - Setting the navBar
-
-        let navBar = UINavigationBar(frame: CGRect(x: 0, y: statusBarHeight, width: screenWidth, height: 44))
-        navBar.barTintColor = StyleSheet.Color.TabBar.background
-
-        navBar.isTranslucent = false
-
-        navBar.tintColor = .white
-
-         // MARK: - Setting navBar Items
-
-        let navItem = UINavigationItem(title: "Blacklist")
-        let settingsItem = UIBarButtonItem(image: UIImage(named: "settings"), style: .done, target: nil, action: #selector(self.settingsButtonAction))
-        let backItem = UIBarButtonItem(image: UIImage(named: "back"), style: .done, target: nil, action: #selector(self.backButtonAction))
-
-        navItem.rightBarButtonItem = settingsItem
-        navItem.leftBarButtonItem = backItem
-        navBar.titleTextAttributes = [
+        blacklistNavigationBar = UINavigationBar(frame: CGRect(x: 0, y: statusBarHeight, width: screenWidth, height: 44))
+        blacklistNavigationBar?.barTintColor = StyleSheet.Color.TabBar.background
+        blacklistNavigationBar?.isTranslucent = false
+        blacklistNavigationBar?.tintColor = .white
+        blacklistNavigationBar?.titleTextAttributes = [
             NSAttributedStringKey.font: UIFont(name: navBarTitleFont, size: 25.0)!,
             NSAttributedStringKey.foregroundColor: UIColor.white
         ]
 
-        navBar.setItems([navItem], animated: false)
+        // MARK: - Setting navBar Items
+        blacklistNavigationItem = UINavigationItem(title: "Blacklist")
 
-        self.view.addSubview(navBar)
+        let settingsItem = UIBarButtonItem(
+            image: UIImage(named: "settings"),
+            style: .done,
+            target: nil,
+            action: #selector(self.settingsButtonAction)
+        )
+
+        if let blacklistNavigationBar = blacklistNavigationBar, let blacklistNavigationItem = blacklistNavigationItem {
+            blacklistNavigationItem.rightBarButtonItem = settingsItem
+            blacklistNavigationBar.setItems([blacklistNavigationItem], animated: false)
+
+            self.view.addSubview(blacklistNavigationBar)
+        }
     }
 
     @objc func backButtonAction() {
@@ -77,5 +81,40 @@ class BlackListNavBar: UINavigationController {
 
     @objc func settingsButtonAction() {
         print("los Ajustos los ajustes!!!")
+    }
+
+    fileprivate func addBackItem() {
+        guard let blacklistNavigationItem = blacklistNavigationItem else {
+            return
+        }
+
+        blacklistNavigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "back"),
+            style: .done,
+            target: nil,
+            action: #selector(self.backButtonAction)
+        )
+    }
+
+    fileprivate func removeBackItem() {
+        guard let blacklistNavigationItem = blacklistNavigationItem else {
+            return
+        }
+
+        blacklistNavigationItem.leftBarButtonItem = nil
+    }
+}
+
+extension BlackListNavBar: UINavigationControllerDelegate {
+    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        guard let rootViewController = navigationController.viewControllers.first else { return }
+
+        if viewController == rootViewController {
+            // Let's hide the back button
+            removeBackItem()
+        } else {
+            // Let's show the back button
+            addBackItem()
+        }
     }
 }
